68c9d42d9b0c5dd3e5f5431d8c6219448a1d66c5
Emerick Bosch <xpc.dev@gmail.com>
2020-02-22 22:15:51 +0200

    Remove snapshot test
    It was overkill - this test is simple and has fixed inputs.

--- tst/binaryTree.test.ts
+++ tst/binaryTree.test.ts
@@ -1,37 +1,37 @@
 import { BinaryTree } from "../src/binaryTree";
 
 const compareNumbers = (a: number, b: number) => {
-  if (a > b) {
-    return "smaller";
-  }
-  if (a < b) {
-    return "larger";
-  }
-  return "equal";
+    if (a > b) {
+        return "smaller";
+    }
+    if (a < b) {
+        return "larger";
+    }
+    return "equal";
 };
 
 describe("BinaryTree", () => {
-  const tree = new BinaryTree<number>(compareNumbers);
+    const tree = new BinaryTree<number>(compareNumbers);
 
-  it("should store unique elements", () => {
-    tree
-      .insert(2)
-      .insert(8)
-      .insert(3)
-      .insert(2)
-      .insert(6);
-    expect(tree.size).toEqual(4);
-  });
+    it("should store unique elements", () => {
+        tree
+            .insert(2)
+            .insert(8)
+            .insert(3)
+            .insert(2)
+            .insert(6);
+        expect(tree.size).toEqual(4);
+    });
 
-  it("should print elements in order", () => {
-    tree
-      .insert(2)
-      .insert(8)
-      .insert(3)
-      .insert(2)
-      .insert(6);
-    expect(tree.toString()).toMatchInlineSnapshot(
-      `"{\\"size\\":4,\\"values\\":[2,3,6,8]}"`
-    );
-  });
+    it("should print elements in order", () => {
+        tree
+            .insert(2)
+            .insert(8)
+            .insert(3)
+            .insert(2)
+            .insert(6);
+        expect(tree.toString()).toEqual(
+            `{\"size\":4,\"values\":[2,3,6,8]}`
+        );
+    });
 });
